cb34ecc09151144b2c6100ce7b33894f712bc6fa
Jeremey Schrack <jdschrack@gmail.com>
2020-09-15 11:06:46 -0400

    Adding Checkbox

--- App.tsx
+++ App.tsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, StyleSheet, StatusBar} from 'react-native';
-import PhoneInput from './app/components/phoneInput/PhoneInput';
+import CheckBox from './app/components/checkbox/CheckBox';
 
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
-  const bgColor = '#fff';
-  const styles = StyleSheet.create({
-    headLine: {
-      fontSize: 32,
-    },
-    safeAreaView: {
-      backgroundColor: bgColor,
-      margin: 0,
-      flex: 1,
-      flexDirection: 'column',
-    },
-    container: {
-      padding: 18,
-    },
-  });
+   const bgColor = '#fff';
+   const styles = StyleSheet.create({
+      headLine: {
+         fontSize: 32,
+      },
+      safeAreaView: {
+         backgroundColor: bgColor,
+         margin: 0,
+         flex: 1,
+         flexDirection: 'column',
+      },
+      container: {
+         padding: 18,
+      },
+   });
 
-  return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <StatusBar barStyle="light-content" />
-      <View style={styles.container}>
+   const [checked, setChecked] = useState<boolean>(false);
+
+   const handleCheck = () => {
+      setChecked(!checked);
+   };
+
+   return (
+      <SafeAreaView style={styles.safeAreaView}>
+         <StatusBar barStyle='light-content' />
+         {/* <View style={styles.container}>
         <PhoneInput />
-      </View>
-    </SafeAreaView>
-  );
+      </View> */}
+         <View style={styles.container}>
+            <CheckBox
+               selected={checked}
+               handleCheck={handleCheck}
+               label={'Set as preferred payment method'}
+            />
+         </View>
+      </SafeAreaView>
+   );
 };
 
 export default App;
